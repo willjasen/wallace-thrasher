cc67958f38a3294e1269008aef26f8ec8477b26d
willjasen <brandon@willjasen.com>
2024-09-13 06:17:40 -0400

    simply the json data
    was having to deal with two copies but now only one is required

---
+++ jekyll/_plugins/load_json_data.rb
@@ -0,0 +1,29 @@
+require 'json'
+
+module Jekyll
+  class LoadJsonData < Generator
+    safe true
+    priority :highest
+
+    def generate(site)
+      # Load JSON files from /assets/json
+      json_dir = File.join(site.source, 'assets', 'json')
+
+      Dir.glob(File.join(json_dir, '*.json')) do |file|
+        data_key = File.basename(file, '.json')
+        json_data = JSON.parse(File.read(file))
+        site.data[data_key] = json_data
+      end
+
+      # Load /assets/data.json
+      data_file = File.join(site.source, 'assets', 'data.json')
+
+      if File.exist?(data_file)
+        data_json = JSON.parse(File.read(data_file))
+        site.data['albums'] = data_json
+      else
+        puts "Warning: data.json not found at #{data_file}"
+      end
+    end
+  end
+end
