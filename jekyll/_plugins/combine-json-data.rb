9b1163d9d0df61af61a31794284cb2e568a5d9b2
willjasen <brandon@willjasen.com>
2025-03-09 00:13:55 -0500

    stashing

--- jekyll/_plugins/combine-json-data.rb
+++ jekyll/_plugins/combine-json-data.rb
@@ -10,7 +10,7 @@ module Jekyll
       combined_data = []
       data_yml_path = File.join(site.source, '_data', 'data.yml')
       yml_data = YAML.load_file(data_yml_path)
-      data_json_path = File.join(site.source, 'assets', 'data.json')
+      data_json_path = File.join(site.source, 'assets', 'json', 'data.json')
       data_json = JSON.parse(File.read(data_json_path))
       
       albums_data = yml_data['Albums']
@@ -18,10 +18,6 @@ module Jekyll
         album_slug = Jekyll::Utils.slugify(album_data['Album'])
         album_data['Tracks'].each do |track_data|
           track_json_path = track_data['Track_JSONPath']
-          if track_json_path.nil? || track_json_path.empty?
-            puts "Warning: track_json_path is empty for track_data: #{track_data.to_s.slice(0, 100)}"
-            # next  # Skip processing for this track
-          end
           track_data_path = File.join(site.source, '_site', 'assets', 'json', album_slug, track_json_path)
           
           if File.exist?(track_data_path)
@@ -29,7 +25,8 @@ module Jekyll
               track_data_content = JSON.parse(File.read(track_data_path))
               # Assign the parsed JSON as a new Tracks key into the current track
               track_data["Subtitles"] = track_data_content
-              combined_data << track_data
+              album_data["Tracks"] = track_data
+              combined_data << album_data
             rescue JSON::ParserError => e
               puts "Error parsing JSON for track_data_path: #{track_data_path}, error: #{e.message.slice(0, 100)}"
             rescue => e
@@ -41,9 +38,9 @@ module Jekyll
         end
       end
 
-      combined_data_path = File.join(site.source, 'assets', 'combined_data.json')
+      combined_data_path = File.join(site.source, 'assets', 'json', 'combined_data.json')
       File.open(combined_data_path, 'w') do |file|
-        file.write(JSON.pretty_generate(combined_data))
+      file.write(JSON.pretty_generate({ "Albums" => combined_data }))
       end
     end
   end
--- jekyll/_plugins/update_yml.rb
+++ jekyll/_plugins/update_yml.rb
@@ -3,7 +3,7 @@ require 'yaml'
 module Jekyll
   class UpdateTracks < Generator
     def generate(site)
-      data_file = File.join(site.source, "assets", 'data.json') 
+      data_file = File.join(site.source, "assets", 'json', 'data.json') 
       json_data = JSON.parse(File.read(data_file))
       updated_yaml = json_data.to_yaml
       target_file = File.join(site.source, '_data', 'data.yml')
