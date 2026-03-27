3ebbfe46702ab5a96d704766fa266e19b111e813
willjasen <willjasen@users.noreply.github.com>
2026-03-27 19:09:20 +0000

    deploy: 0a3f70fb99c0e48f6b5073d958e6f86b96dcb0b7

--- assets/js/search.js
+++ assets/js/search.js
@@ -5,7 +5,7 @@ const BASE_URL = '/wallace-thrasher';
 console.log("BASE_URL: " + (BASE_URL ? BASE_URL : "<null>"));
 const loadIndividualTrackJSON = 'false' === 'true';
 console.log("loadIndividualTrackJSON: " + loadIndividualTrackJSON);
-const BUILD_TIMESTAMP = '1774637762';
+const BUILD_TIMESTAMP = '1774638546';
 
 /*
     This function retrieves a JSON document from a given path
